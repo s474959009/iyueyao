7362827aa32f1982cfbaf4c4366d68b8a403235b
yueyao <409355439@qq.com>
2013-12-03 22:21:38 +0800

    add markdown editor

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,16 @@
 /*
  * GET home page.
  */
+var admin = require('./admin')
+	,topic = require('./topic')
+	,blog = require('./blog');
 
-exports.index = function(req, res){
-  res.render('index', { title: '测试' });
+module.exports = function(app){
+	app.get('/',function(req,res){
+		res.render('index', { title: '测试' });
+	});
+
+	app.get('/admin',admin);
+	app.get('/topic',topic);
+	app.get('/blog',blog);
 };
