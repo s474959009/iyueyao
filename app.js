5321d8b67b8ba8d8839f0120db8183debad74dd6
yueyao <409355439@qq.com>
2013-12-04 22:47:32 +0800

    add reg login module

--- app.js
+++ app.js
@@ -9,16 +9,30 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var MongoStore = require('connect-mongo')(express);
+var settings = require('./setting');
+var flash = require('connect-flash');
+var User = require('./models/user') ;
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 8888);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(flash());
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({
+	secret: settings.cookieSecret,
+	key: settings.db,//cookie name
+	cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+	store: new MongoStore({
+		db: settings.db
+	})
+}));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,6 +43,7 @@ if ('development' == app.get('env')) {
 
 /*app.get('/', routes.index);
 app.get('/users', user.list);*/
+
 routes(app);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,104 @@
 /*
  * GET home page.
  */
+var crypto = require('crypto')
+	,flash = require('connect-flash');
+
 var admin = require('./admin')
 	,topic = require('./topic')
-	,blog = require('./blog');
+	,blog = require('./blog')
+	,User = require('../models/user')
+	,Post = require('../models/post.js');
 
 module.exports = function(app){
-	app.get('/',function(req,res){
-		res.render('index', { title: '测试' });
+	app.get('/', function (req, res) {
+		res.render('index', {
+			title: '主页',
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		});
+	});
+
+	app.get('/reg', function (req, res) {
+		res.render('reg', {
+			title: '注册',
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		});
+	});
+
+	app.post('/reg', function (req, res) {
+		var name = req.body.name,
+			password = req.body.password,
+			password_re = req.body['password-repeat'];
+		if (password_re != password) {
+			req.flash('error', '两次输入的密码不一致!');
+			return res.redirect('/reg');
+		}
+		var md5 = crypto.createHash('md5'),
+			password = md5.update(req.body.password).digest('hex');
+		var newUser = new User({
+			name: req.body.name,
+			password: password,
+			email: req.body.email
+		});
+		User.get(newUser.name, function (err, user) {
+			if (user) {
+				req.flash('error', '用户已存在!');
+				return res.redirect('/reg');
+			}
+			newUser.save(function (err, user) {
+				if (err) {
+					req.flash('error', err);
+					return res.redirect('/reg');
+				}
+				req.session.user = user;
+				req.flash('success', '注册成功!');
+				res.redirect('/');
+			});
+		});
+	});
+
+	app.post('/login',function(req,res){
+		var md5 = crypto.createHash('md5'),
+			password = md5.update(req.body.password).digest('hex');
+
+		User.get(req.body.name, function (err, user) {
+			if (!user) {
+				req.flash('error', '用户不存在!');
+				return res.redirect('/login');
+			}
+
+			if (user.password != password) {
+				req.flash('error', '密码错误!');
+				return res.redirect('/login');
+			}
+			req.session.user = user;
+			req.flash('success', '登陆成功!');
+			res.redirect('/');
+		});
+	});
+  app.get("/login",function(req,res){
+	  res.render('login',{title:'login'});
+  });
+
+
+
+
+
+	app.post('/post', function (req, res) {
+		var currentUser = req.session.user,
+			post = new Post(currentUser.name, req.body.title, req.body.post);
+			post.save(function (err) {
+				if (err) {
+					req.flash('error', err);
+					return res.redirect('/');
+				}
+				req.flash('success', '发布成功!');
+				res.redirect('/');//发表成功跳转到主页
+			});
 	});
 
 	app.get('/admin',admin);
